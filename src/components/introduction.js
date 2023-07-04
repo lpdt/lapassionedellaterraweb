d9671d99885cb2bcb09748ac5a0222a0bfa7ac45
Teygeta <vitto.gioda@outlook.it>
2023-07-04 20:53:55 +0200

    add icons

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import background from "../public/background.png"
+import background from "~/public/background.png"
 export default function Introduction() {
 
   return (
