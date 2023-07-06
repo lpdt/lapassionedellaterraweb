18440909d8ee6870acfb5a3a6b943c61365e8f35
Teygeta <vitto.gioda@outlook.it>
2023-07-06 12:42:06 +0200

    new image import

--- src/components/photos.js
+++ src/components/photos.js
@@ -1,13 +1,24 @@
-import photoIcon from '~/public/photo.svg'
 import Image from 'next/image'
+import cavoli from '~/public/img/cavoli.webp'
 import cavolini from '~/public/img/cavolini.webp'
 import fagioli from '~/public/img/fagioli.webp'
+import fioridiz from '~/public/img/fioridiz.webp'
+import insalata from '~/public/img/insalata.webp'
+import melanzana from '~/public/img/melanzana.webp'
+import mele from '~/public/img/mele.webp'
+import peperoni from '~/public/img/peperoni.webp'
 import pomodori from '~/public/img/pomodori.webp'
+import pomodorini from '~/public/img/pomodorini.webp'
+import uva from '~/public/img/uva.webp'
+import verdurine from '~/public/img/verdurine.webp'
 import zucchine from '~/public/img/zucchine.webp'
+import photoIcon from '~/public/photo.svg'
 
 export default function Photos() {
 
-  const images = [fagioli, cavolini, pomodori, zucchine]
+  const images = [
+    fagioli, cavolini, pomodori, zucchine, cavoli, fioridiz, insalata, melanzana, mele, peperoni, pomodorini, uva, verdurine
+  ]
 
 
   return (
