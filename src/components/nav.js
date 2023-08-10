f814be44e3eea8f8dbd3e1811bdac355f2cfcdce
Teygeta <vitto.gioda@outlook.it>
2023-08-10 11:45:14 +0200

    fix: navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -96,7 +96,7 @@ export default function Nav() {
         </nav>
         <Dialog as='div' className='lg:hidden' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className='fixed inset-0 z-10' />
-          <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-default-yellow sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+          <Dialog.Panel className='fixed inset-y-0 right-0 z-10 z-50 w-full px-6 py-6 overflow-y-auto bg-default-yellow sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
             <div className='flex items-center justify-end'>
               <button
                 type='button'
